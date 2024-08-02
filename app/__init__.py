6dface8664d0316c45a3b3bf6228c0974c26e46f
Shaivin <sagarshaivin07@gmail.com>
2024-08-02 11:22:56 +0530

    Updated Python app committed

--- app/__init__.py
+++ app/__init__.py
@@ -13,15 +13,21 @@ def helloworld():
 def hello():
     html = "<h3>Hello World from {hostname}!</h3>"
     html += "<h3>Your random word is: {random_word}</h3>"
+
     db = mysql.connector.connect(
-              host=os.getenv("MYSQL_SERVICE_HOST"),
-              port=os.getenv("MYSQL_SERVICE_PORT"),
-              user="root",
-              passwd=os.getenv("MYSQL_DB_PASSWORD"),
-              database="pyapplicationdb",
-              auth_plugin="mysql_native_password"
-         )
+        host=os.getenv("MYSQL_SERVICE_HOST", "localhost"),
+        port=int(os.getenv("MYSQL_SERVICE_PORT", 3306)),  # Default to 3306 if not set
+        user="root",  # Change if using a different user
+        passwd=os.getenv("MYSQL_DB_PASSWORD"),
+        database="pyapplicationdb",
+        auth_plugin="mysql_native_password"
+    )
+
     cursor = db.cursor()
-    cursor.execute("select word from random_words order by rand() limit 1;")
+    cursor.execute("SELECT word FROM random_words ORDER BY RAND() LIMIT 1;")
     res = cursor.fetchall()
+
     return html.format(random_word=res[0][0], hostname=socket.gethostname())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
